7d41464e6b9d0696f6a79a1816f9e2807ca1b1fe
“windy” <418809213@qq.com>
2021-08-04 18:01:56 +0800

    the first hello world

---
+++ src/main/java/HelloJenkins.java
@@ -0,0 +1,11 @@
+/**
+ * @Project: jenkins
+ * @Author: 阿线现
+ * @Create: 2021-08-04 17:59
+ * @Desc：
+ **/
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+    }
+}
